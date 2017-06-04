455bf54622c1980b183f8d932a8b4e0fb151c98c
Jarryd Stamatelos <jarryd.stamatelos@gmail.com>
2017-06-04 00:18:40 -0400

    Button linked

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { PartnerPage } from '../pages/list/list';
 import { OrderForm } from '../pages/orderform/orderform';
+import { ProductForm } from '../pages/productform/productform'; 
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -15,7 +16,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     HomePage,
     PartnerPage,
-    OrderForm
+    OrderForm,
+    ProductForm
   ],
   imports: [
     BrowserModule,
@@ -26,7 +28,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     HomePage,
     PartnerPage,
-    OrderForm
+    OrderForm,
+    ProductForm
   ],
   providers: [
     StatusBar,
