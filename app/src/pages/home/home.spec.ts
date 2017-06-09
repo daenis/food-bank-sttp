85de9b123a89240a873e69772e3f2820c5d5936d
Aaron Long <aaron.long.c@gmail.com>
2017-06-09 09:32:50 -0400

    Resolved Linting issues with test

--- app/src/pages/home/home.spec.ts
+++ app/src/pages/home/home.spec.ts
@@ -1,17 +1,18 @@
-import { TestBed, async } from '@angular/core/testing';
-import { NavController, IonicModule } from 'ionic-angular';
+import { async } from '@angular/core/testing';
 import { HomePage } from './home';
 import { TestUtils } from '../../test';
 
 describe('HomePage', () => {
-  let fixture: any;
-  let instance: any;
-  beforeEach(async(() => TestUtils.beforeEachCompiler([HomePage]).then(compiled => {
-    fixture = compiled.fixture;
-    instance = compiled.instance;
-  })));
-  it('should create the app', async(() => {
-    const home = fixture.debugElement.componentInstance;
-    expect(home).toBeTruthy();
-  }));
+	let fixture: any;
+	let instance: any;
+
+	beforeEach(async(() => TestUtils.beforeEachCompiler([HomePage]).then(compiled => {
+		fixture = compiled.fixture;
+		instance = compiled.instance;
+	})));
+
+	it('should create the app', async(() => {
+		const home = fixture.debugElement.componentInstance;
+		expect(home).toBeTruthy();
+	}));
 })
