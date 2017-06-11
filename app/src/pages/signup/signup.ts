42203b1286d8f3d0ad566a95640c701aa4c21ff9
Aaron Long <aaron.long.c@gmail.com>
2017-06-11 01:27:03 -0400

    Made signup persist to mock database

--- app/src/pages/signup/signup.ts
+++ app/src/pages/signup/signup.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Http } from '@angular/http';
+import { environment } from '../../environments/environment'
 
 @Component({
 	selector: 'page-signup',
@@ -18,7 +19,7 @@ export class SignUp implements OnInit {
 			name: ['', Validators.minLength(2)],
 			userName: ['', Validators.minLength(5)],
 			email: ['',
-				Validators.pattern('^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$')],
+				Validators.required],
 			password: ['', Validators.required],
 			phoneNumber: ['', Validators.required],
 			address: ['']
@@ -31,6 +32,23 @@ export class SignUp implements OnInit {
 
 	public submit(): void {
 		if (this.form.valid) {
+			const result = this.parseForm(this.form.getRawValue())
+			const uri = environment.uri + 'users/'
+			this.http.post(uri, result).toPromise()
+			         .then(e => this.goBack())
+					 .catch(e => console.warn(e))
 		}
+	}
+
+	private parseForm(form: Object): Object {
+		return ({
+			name: form['name'],
+			username: form['username'],
+			address: form['address'],
+			email: form['email'],
+			password: form['password'],
+			phone: form['phoneNumber'],
+			type: 'volunteers'
+		})
 	}
 }
