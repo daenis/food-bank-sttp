039e761adc944c9a9d3a13fa37d92a055a1e47bb
Aaron Long <aaron.long.c@gmail.com>
2017-06-12 08:55:03 -0400

    Began Login refactor

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -1,27 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { VolunteerSignUp } from '../volunteersignup/volunteersignup';
 import { PartnerSignUp } from '../partnersignup/partnersignup';
 import { SignUp } from '../signup/signup';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'page-home',
 	templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-	constructor(public navCtrl: NavController) {
-		
-	}
-	goToVolunteerSignUp(){
+	private login: FormGroup;
+
+	constructor(public navCtrl: NavController,
+							private fb: FormBuilder) {}
+
+	public goToVolunteerSignUp(): void {
 		this.navCtrl.push(VolunteerSignUp);
 	}
-	goToPartnerSignUp(){
+	public goToPartnerSignUp(): void {
 		this.navCtrl.push(PartnerSignUp);
 	}
 
-	goToSignUp() {
+	public goToSignUp(): void {
 		this.navCtrl.push(SignUp);
 	}
 
+	public ngOnInit(): void {
+		this.login = this.fb.group({
+			username: ['', Validators.required],
+			password: ['', Validators.required]
+		})
+	}
+
+	public authenticate(): void {
+		if (this.login.valid) {
+			console.log(this.login.getRawValue())
+		}
+	}
 }
