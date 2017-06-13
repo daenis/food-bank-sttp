1f8cb0990663bc5b06eaf2a3c2883792e2b05d6a
Aaron Long <aaron.long.c@gmail.com>
2017-06-13 12:03:34 -0400

    Began Login Feature

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -4,6 +4,10 @@ import { VolunteerSignUp } from '../volunteersignup/volunteersignup';
 import { PartnerSignUp } from '../partnersignup/partnersignup';
 import { SignUp } from '../signup/signup';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Http, Response } from '@angular/http';
+import { environment } from '../../environments/environment';
+import { Auth } from '../../app/auth.service'
+import 'rxjs/add/operator/toPromise';
 
 @Component({
 	selector: 'page-home',
@@ -14,7 +18,9 @@ export class HomePage implements OnInit {
 	private login: FormGroup;
 
 	constructor(public navCtrl: NavController,
-							private fb: FormBuilder) {}
+				private fb: FormBuilder,
+				private http: Http,
+				private auth: Auth) {}
 
 	public goToVolunteerSignUp(): void {
 		this.navCtrl.push(VolunteerSignUp);
@@ -36,7 +42,12 @@ export class HomePage implements OnInit {
 
 	public authenticate(): void {
 		if (this.login.valid) {
-			console.log(this.login.getRawValue())
+			const auth = this.login.getRawValue();
+			const uri = environment.uri + 'users?username=' + auth.username;
+			this.http.get(uri, auth).toPromise()
+			.then((result: Response) => result.json())
+			.then((json: Object) => this.auth.addUser(json[0]))
+			.catch(e => console.log(e))
 		}
 	}
 }
