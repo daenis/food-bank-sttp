3ccae59430fc23587d908244d8fdf1a6a09f220d
Aaron Long <aaron.long.c@gmail.com>
2017-06-14 18:56:13 -0400

    Cleanup unused files && fixed formatting

--- app/src/pages/fooditem/fooditem.spec.ts
+++ app/src/pages/fooditem/fooditem.spec.ts
@@ -4,14 +4,14 @@ import { FoodItem } from './fooditem';
 import { TestUtils } from '../../test';
 
 describe('FoodItem', () => {
-  let fixture: any;
-  let instance: any;
-  beforeEach(async(() => TestUtils.beforeEachCompiler([FoodItem]).then(compiled => {
-    fixture = compiled.fixture;
-    instance = compiled.instance;
-  })));
-  it('should be able to load', async(() => {
-    const fooditem = fixture.debugElement.componentInstance;
-    expect(fooditem).toBeTruthy();
-  }));
+	let fixture: any;
+	let instance: any;
+	beforeEach(async(() => TestUtils.beforeEachCompiler([FoodItem]).then(compiled => {
+		fixture = compiled.fixture;
+		instance = compiled.instance;
+	})));
+	it('should be able to load', async(() => {
+		const fooditem = fixture.debugElement.componentInstance;
+		expect(fooditem).toBeTruthy();
+	}));
 })
