8b613464a5cef8fcff9584d389bd4ece0047b89b
Jarryd Stamatelos <jarryd.stamatelos@gmail.com>
2017-06-23 22:16:30 -0400

    Forms not throwing error anymore

--- app/src/pages/product/product-list.component.ts
+++ app/src/pages/product/product-list.component.ts
@@ -3,30 +3,55 @@ import { NavController, NavParams } from 'ionic-angular';
 import { Product } from './product';
 import { ProductService } from './product.service';
 //import { ProductDetailComponent } from './product-detail.component';
-import { FoodItem } from '../fooditem/fooditem'
+import { FoodItem } from '../fooditem/fooditem';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-		templateUrl: './product-list.component.html',
-		providers: [ProductService]
+	templateUrl: './product-list.component.html',
+	providers: [ProductService]
 })
 export class ProductListComponent implements OnInit {
-		private pageTitle: string = 'Categories';
-		private errorMessage: string;
-		private products: String[];
+	private pageTitle: string = 'Categories';
+	private errorMessage: string;
+	private products: String[];
+	private form: FormGroup;
 
-		constructor(
-			private _productService: ProductService,
-			private navCtrl: NavController,
-			private navParams: NavParams) {
-		}
-		public ngOnInit(): void {
-				this._productService.getProducts()
-								.subscribe(products => this.products = products,
-														error => this.errorMessage = <any> error);
-		}
-		public goToProductDetail(category: string) {
-			console.log(category)
-			this.navCtrl.push(FoodItem, {category});
-		}
+	constructor(
+		private _productService: ProductService,
+		private navCtrl: NavController,
+		private navParams: NavParams,
+		private fb: FormBuilder) {
+	}
+	
+	public ngOnInit(): void {
+		this.getForm();
+		this._productService.getProducts()
+			.subscribe(products => this.products = products,
+			error => this.errorMessage = <any>error);
+	}
+
+	public goToProductDetail(category: string) {
+		const formData = this.parseForm(this.form.getRawValue());
+		console.log(category)
+		this.navCtrl.push(FoodItem, { category }, formData);
+	}
+
+	public getForm() {
+		this.form = this.fb.group({
+			description: [''],
+			quantity: ['']
+		})
+	}
+
+	private parseForm(form: Object): Object {
+		return ({
+			description: form['description'],
+			quantity: form['quantity']
+		})
+	}
+
+	public goBack(): void {
+		this.navCtrl.pop();
+	}
 
 }
