aad8f684c757f0d7b2a042ac61802eb373489330
Jarryd Stamatelos <jarryd.stamatelos@gmail.com>
2017-06-24 14:05:48 -0400

    Posts displaying

--- app/src/pages/product/product-list.component.ts
+++ app/src/pages/product/product-list.component.ts
@@ -5,6 +5,9 @@ import { ProductService } from './product.service';
 //import { ProductDetailComponent } from './product-detail.component';
 import { FoodItem } from '../fooditem/fooditem';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Http } from '@angular/http';
+import { environment } from '../../environments/environment'
+import { OrderForm } from '../orderform/orderform';
 
 @Component({
 	templateUrl: './product-list.component.html',
@@ -20,25 +23,33 @@ export class ProductListComponent implements OnInit {
 		private _productService: ProductService,
 		private navCtrl: NavController,
 		private navParams: NavParams,
-		private fb: FormBuilder) {
-	}
-	
-	public ngOnInit(): void {
-		this.getForm();
-		this._productService.getProducts()
-			.subscribe(products => this.products = products,
-			error => this.errorMessage = <any>error);
+		private fb: FormBuilder,
+		private http: Http) {
 	}
 
-	public goToProductDetail(category: string) {
-		const form = this.parseForm(this.form.getRawValue());
-		console.log(form)
-		console.log(category)
-		this.navCtrl.push(FoodItem, { category }, form);
+	public ngOnInit(): void {
+		this.getForm();
+		// this._productService.getProducts()
+		// 	.subscribe(products => this.products = products,
+		// 	error => this.errorMessage = <any>error);
+	}
+
+	// public goToProductDetail(category: string) {
+	// 	const form = this.parseForm(this.form.getRawValue());
+	// 	console.log(form)
+	// 	console.log(category)
+	// 	this.navCtrl.push(FoodItem, { category }, form);
+	// }
+	public submit(): void {
+			const result = this.parseForm(this.form.getRawValue());
+			const uri = environment.uri + 'api/order';
+			this.http.put(uri, result).toPromise()
+				.catch(e => console.warn(e))
 	}
 
 	public getForm() {
 		this.form = this.fb.group({
+			category: [''],
 			description: [''],
 			quantity: ['']
 		})
@@ -46,6 +57,7 @@ export class ProductListComponent implements OnInit {
 
 	private parseForm(form: Object): Object {
 		return ({
+			category: form['category'],
 			description: form['description'],
 			quantity: form['quantity']
 		})
@@ -53,6 +65,10 @@ export class ProductListComponent implements OnInit {
 
 	public goBack(): void {
 		this.navCtrl.pop();
+	}
+
+	public goToOrderForm() {
+		this.navCtrl.push(OrderForm);
 	}
 
 }
