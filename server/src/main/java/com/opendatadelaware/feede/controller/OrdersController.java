d304f2d4ff69e84456340baa4dbaf91f4d6a6e64
Mark A Brown <markabrown87@gmail.com>
2017-07-02 11:12:07 -0400

    Added repo that will be deleted later

--- server/src/main/java/com/opendatadelaware/feede/controller/OrdersController.java
+++ server/src/main/java/com/opendatadelaware/feede/controller/OrdersController.java
@@ -1,12 +1,16 @@
 package com.opendatadelaware.feede.controller;
 
 import com.opendatadelaware.feede.model.Orders;
+import com.opendatadelaware.feede.repository.OrderRepository;
 import com.opendatadelaware.feede.service.OrdersService;
 import com.opendatadelaware.feede.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 /**
  * Created by markbrown on 6/29/17.
@@ -22,6 +26,32 @@ public class OrdersController {
     public void setOrderService(OrdersService orderService) {
         service = orderService;
     }
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ResponseEntity<?> postOrder(@RequestBody Orders order) {
+        return new ResponseEntity<>(orderRepository.save(order), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{uuid}", method = RequestMethod.GET)
+    public ResponseEntity<?> getOrder(@PathVariable UUID uuid) {
+        return new ResponseEntity<>(orderRepository.findOne(uuid), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{uuid}", method = RequestMethod.PUT)
+    public ResponseEntity<?> editOrder(@RequestBody Orders order, @PathVariable UUID uuid) {
+        return new ResponseEntity<>(orderRepository.save(order), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{uuid}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteOrder(@PathVariable UUID uuid) {
+        return new ResponseEntity<>(orderRepository.delete(uuid), HttpStatus.OK);
+    }
+
+
+
 
 
 
