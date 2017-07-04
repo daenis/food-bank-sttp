938b7fabad11505216f2f627a515426cab836ec3
Aaron Long <aaron.long.c@gmail.com>
2017-07-04 09:22:42 -0400

    Started on JWT security

--- server/src/main/java/com/opendatadelaware/feede/config/WebSecurityConfig.java
+++ server/src/main/java/com/opendatadelaware/feede/config/WebSecurityConfig.java
@@ -1,12 +1,24 @@
 package com.opendatadelaware.feede.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Created by aaronlong on 6/28/17.
  */
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().ignoringAntMatchers("/api/user");
+
+    http.authorizeRequests()
+            .antMatchers("/api/user")
+            .permitAll()
+            .antMatchers("/**/*")
+            .denyAll();
+  }
 }
