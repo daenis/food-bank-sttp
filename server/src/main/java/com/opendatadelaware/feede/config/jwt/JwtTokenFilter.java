020f14289b73c4607046bfc81b8b0bfc4584b862
Dennis Kalaygian <daeniskalaygian@gmail.com>
2017-07-05 15:23:54 -0400

    Finished filter

--- server/src/main/java/com/opendatadelaware/feede/config/jwt/JwtTokenFilter.java
+++ server/src/main/java/com/opendatadelaware/feede/config/jwt/JwtTokenFilter.java
@@ -1,10 +1,16 @@
 package com.opendatadelaware.feede.config.jwt;
 
 import com.opendatadelaware.feede.config.WebSecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,9 +20,12 @@ import java.io.IOException;
  * Created by denniskalaygian on 7/5/17.
  */
 public class JwtTokenFilter extends AbstractAuthenticationProcessingFilter {
+    private final AuthenticationFailureHandler failureHandler;
 
-    public JwtTokenFilter() {
-        super("api/user");
+    @Autowired
+    public JwtTokenFilter(AuthenticationFailureHandler failureHandler, RequestMatcher matcher) {
+        super(matcher);
+        this.failureHandler = failureHandler;
     }
 
     @Override
@@ -27,5 +36,22 @@ public class JwtTokenFilter extends AbstractAuthenticationProcessingFilter {
         String tokenHeader = httpServletRequest.getHeader(WebSecurityConfig.JWT_TOKEN_HEADER_PARAM);
         JwtToken token = JwtToken.createTokenInstanceFromHeader(tokenHeader);
         return getAuthenticationManager().authenticate(new JwtAuthenticationToken(token));
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authResult);
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request,
+                                              HttpServletResponse response,
+                                              AuthenticationException failed) throws IOException, ServletException {
+        failureHandler.onAuthenticationFailure(request, response, failed);
     }
 }
