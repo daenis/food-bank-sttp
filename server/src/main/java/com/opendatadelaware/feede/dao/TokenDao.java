8d68d043237a66efdaeac6cd1f232bd0b62b252c
Aaron Long <aaron.long.c@gmail.com>
2017-07-05 11:08:02 -0400

    Added Token Lookup

--- server/src/main/java/com/opendatadelaware/feede/dao/TokenDao.java
+++ server/src/main/java/com/opendatadelaware/feede/dao/TokenDao.java
@@ -1,14 +1,44 @@
 package com.opendatadelaware.feede.dao;
 
 import com.opendatadelaware.feede.model.Token;
+import com.opendatadelaware.feede.model.Users;
+import org.apache.lucene.search.Query;
+import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.jpa.Search;
+import org.hibernate.search.query.dsl.QueryBuilder;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 /**
  * Created by aaronlong on 7/4/17.
  */
 public class TokenDao extends AbstractDao<Token, UUID> {
+  private EntityManager entityManager;
+
   public TokenDao() {
     super(Token.class);
+  }
+
+  @PersistenceContext
+  public void setEntityManager(EntityManager em) {
+    entityManager = em;
+  }
+
+  public Token getUserFromToken(String uuid) {
+    FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(entityManager);
+    final QueryBuilder queryBuilder = fullTextEntityManager
+            .getSearchFactory()
+            .buildQueryBuilder().forEntity(Token.class).get();
+    Query query = queryBuilder
+                          .bool()
+                            .must(queryBuilder.keyword().onField("token").matching(uuid).createQuery())
+                            .must(queryBuilder.keyword().onField("active").matching(true).createQuery())
+                            .must(queryBuilder.range().onField("expiration_date").above(new Date()).createQuery())
+                            .createQuery();
+    return (Token) fullTextEntityManager.createFullTextQuery(query).getSingleResult();
   }
 }
--- server/src/main/java/com/opendatadelaware/feede/model/Token.java
+++ server/src/main/java/com/opendatadelaware/feede/model/Token.java
@@ -1,8 +1,11 @@
 package com.opendatadelaware.feede.model;
 
+import com.opendatadelaware.feede.model.converters.TokenConverter;
+import com.opendatadelaware.feede.model.fields.TokenField;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -34,13 +37,21 @@ public class Token implements Serializable {
   private UUID token;
 
   @Temporal(TemporalType.TIMESTAMP)
-  @Column(name = "date_time", nullable = false, updatable=false)
-  private Date dateTime;
+  @Column(name = "creation_date", nullable = false, updatable=false)
+  private Date creationTime;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(name = "expiration_date", nullable = false, updatable=false)
+  private Date expirationTime;
 
   @ManyToOne(fetch = FetchType.EAGER)
-  @JoinColumn(name = "user", referencedColumnName = "uuid", nullable=false, updatable=false)
+  @JoinColumn(name = "user", referencedColumnName = "uuid", nullable = false, updatable = false)
   private Users user;
 
   @Column(name = "active", nullable = false)
   private boolean active;
+
+  @Convert(converter = TokenConverter.class)
+  @Column(name = "token_type", nullable = false, updatable = false)
+  private TokenField tokenType;
 }
