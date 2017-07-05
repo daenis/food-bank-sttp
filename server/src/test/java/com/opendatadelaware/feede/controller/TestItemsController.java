ecc7c0b5aa43e1d55dccf72a86f18070b4f936dc
Jarryd Stamatelos <jarryd.stamatelos@gmail.com>
2017-07-05 10:27:20 -0400

    stashing to pull

--- server/src/test/java/com/opendatadelaware/feede/controller/TestItemsController.java
+++ server/src/test/java/com/opendatadelaware/feede/controller/TestItemsController.java
@@ -71,5 +71,18 @@ public class TestItemsController {
             return null;
         }
     }
+    @Test
+    public void test_get_by_id_success() throws Exception {
+        Items items = new Items();
+        
+        when(userService.findById(1)).thenReturn(user);
+        mockMvc.perform(get("/users/{id}", 1))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.username", is("Daenerys Targaryen")));
+        verify(userService, times(1)).findById(1);
+        verifyNoMoreInteractions(userService);
+    }
 
 }
