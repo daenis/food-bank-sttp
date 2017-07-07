6395963a2fe18b90fc686ebaad76cd67ac21c378
Dennis Kalaygian <daeniskalaygian@gmail.com>
2017-07-07 10:03:07 -0400

    Created RequiredFields Enum

---
+++ server/src/main/java/com/opendatadelaware/feede/config/jwt/token/RequiredFields.java
@@ -0,0 +1,32 @@
+package com.opendatadelaware.feede.config.jwt.token;
+
+import java.util.EnumSet;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * Created by denniskalaygian on 7/7/17.
+ */
+public enum RequiredFields {
+
+    SUBJECT(""), ISSUED(""), EXPIRATION(""),
+    ISSUER(""), JTI("");
+
+    private String fieldName;
+
+    RequiredFields(String field) {
+        fieldName = field;
+    }
+
+    public String getField() {
+        return fieldName;
+    }
+
+    public static boolean validateFields(Map<String, JwtAttribute> attr) {
+        Optional<RequiredFields> result = EnumSet.allOf(RequiredFields.class)
+                .stream()
+                .filter(f -> !attr.containsKey(f))
+                .findAny();
+        return result.isPresent() ? false : true;
+    }
+}
