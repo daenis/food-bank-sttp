5ecb2af9e9eca02a69047c3a86d1a1b943643e12
Dennis Kalaygian <daeniskalaygian@gmail.com>
2017-07-07 16:08:55 -0400

    Wrote tests for claims

--- server/src/test/java/com/opendatadelaware/feede/config/jwt/token/TestJwtToken.java
+++ server/src/test/java/com/opendatadelaware/feede/config/jwt/token/TestJwtToken.java
@@ -1,9 +1,11 @@
 package com.opendatadelaware.feede.config.jwt.token;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.impl.crypto.MacProvider;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.security.Key;
@@ -14,15 +16,83 @@ import java.util.Date;
  */
 public class TestJwtToken {
 
+    private Key key;
+    private String jwt;
+    private static final String BEARER = "Bearer ";
+
+    @Before
+    public void setup() {
+        key = MacProvider.generateKey();
+        jwt = Jwts.builder().setSubject("johndoe@example.net").
+                setIssuer("tariq").setIssuedAt(new Date()).setExpiration(new Date(new Date().getTime() + 90000))
+                .setAudience("Froilan").setId("re4378t476590876").signWith(SignatureAlgorithm.HS512, key).compact();
+    }
+
     @Test
     public void testCreateTokenInstanceFromHeader() {
-        Key key = MacProvider.generateKey();
-        String jwt = Jwts.builder().setSubject("johndoe@example.net").
-        setIssuer("tariq").setIssuedAt(new Date()).setExpiration(new Date(new Date().getTime() + 90000))
-                .setAudience("Froilan").setId("re4378t476590876").signWith(SignatureAlgorithm.HS512, key).compact();
-        String header = String.format("Bearer %s", jwt);
-        System.out.println(key.getEncoded());
+        String header = BEARER + jwt;
         JwtToken token = JwtToken.createTokenInstanceFromHeader(header, key.getEncoded());
-        Assert.assertEquals("Assert that 'Bearer' was stripped from header correctly", jwt, token.getTokenString());
+        String tokenString = token.getTokenString();
+        Assert.assertEquals("Assert that 'Bearer' was stripped from header correctly", jwt, tokenString);
     }
+
+    @Test
+    public void testGetTokenClaimsSubject() {
+        String header = BEARER + jwt;
+        JwtToken token = JwtToken.createTokenInstanceFromHeader(header, key.getEncoded());
+        Claims claims = token.getTokenClaims();
+        String prediction = "johndoe@example.net";
+        String result = claims.getSubject();
+        Assert.assertEquals("Assert that token subject is generated correctly", prediction, result);
+    }
+
+    @Test
+    public void testGetTokenClaimsIssuer() {
+        String header = BEARER + jwt;
+        JwtToken token = JwtToken.createTokenInstanceFromHeader(header, key.getEncoded());
+        Claims claims = token.getTokenClaims();
+        String prediction = "tariq";
+        String result = claims.getIssuer();
+        Assert.assertEquals("Assert that token issuer is generated correctly", prediction, result);
+    }
+
+    @Test
+    public void testGetTokenClaimsIssuedAt() {
+        String header = BEARER + jwt;
+        JwtToken token = JwtToken.createTokenInstanceFromHeader(header, key.getEncoded());
+        Claims claims = token.getTokenClaims();
+        Date result = claims.getIssuedAt();
+        Assert.assertTrue("Assert that token issue date is generated", result != null);
+    }
+
+    @Test
+    public void testGetTokenClaimsExpiration() {
+        String header = BEARER + jwt;
+        JwtToken token = JwtToken.createTokenInstanceFromHeader(header, key.getEncoded());
+        Claims claims = token.getTokenClaims();
+        Long currentTime = claims.getIssuedAt().getTime();
+        Long expirationTime = claims.getExpiration().getTime();
+        Assert.assertTrue("Assert that token expire date is generated", currentTime < expirationTime);
+    }
+
+    @Test
+    public void testGetTokenClaimsAudience() {
+        String header = BEARER + jwt;
+        JwtToken token = JwtToken.createTokenInstanceFromHeader(header, key.getEncoded());
+        Claims claims = token.getTokenClaims();
+        String prediction = "Froilan";
+        String result = claims.getAudience();
+        Assert.assertEquals("Assert that token audience is generated correctly", prediction, result);
+    }
+
+    @Test
+    public void testGetTokenClaimsId() {
+        String header = BEARER + jwt;
+        JwtToken token = JwtToken.createTokenInstanceFromHeader(header, key.getEncoded());
+        Claims claims = token.getTokenClaims();
+        String prediction = "re4378t476590876";
+        String result = claims.getId();
+        Assert.assertEquals("Assert that token Id is generated correctly", prediction, result);
+    }
+    
 }
