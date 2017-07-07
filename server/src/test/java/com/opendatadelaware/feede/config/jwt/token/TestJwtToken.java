5eac2263b96d1eb0319e35748451733781c657ac
Dennis Kalaygian <daeniskalaygian@gmail.com>
2017-07-07 15:22:59 -0400

    Writing out test for jwttoken.. dying

--- server/src/test/java/com/opendatadelaware/feede/config/jwt/token/TestJwtToken.java
+++ server/src/test/java/com/opendatadelaware/feede/config/jwt/token/TestJwtToken.java
@@ -1,7 +1,21 @@
 package com.opendatadelaware.feede.config.jwt.token;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.crypto.MacProvider;
+import org.junit.Test;
+
+import java.security.Key;
+
 /**
  * Created by denniskalaygian on 7/7/17.
  */
 public class TestJwtToken {
+
+    @Test
+    public void testCreateTokenInstanceFromHeader() {
+        Key key = MacProvider.generateKey();
+        String jwt = Jwts.builder().signWith(SignatureAlgorithm.HS512, key).compact();
+        String header = String.format("Bearer %s", jwt);
+    }
 }
