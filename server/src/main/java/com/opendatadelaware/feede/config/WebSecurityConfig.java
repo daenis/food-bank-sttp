53578dbc3ee7548f0270cb24f4ddf35826d622d8
Aaron Long <aaron.long.c@gmail.com>
2017-07-08 16:51:05 -0400

    Converted WebSecurity Password Encoder to the interface

--- server/src/main/java/com/opendatadelaware/feede/config/WebSecurityConfig.java
+++ server/src/main/java/com/opendatadelaware/feede/config/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -50,7 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   }
 
   @Bean
-  protected BCryptPasswordEncoder passwordEncoder() {
+  protected PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
 
