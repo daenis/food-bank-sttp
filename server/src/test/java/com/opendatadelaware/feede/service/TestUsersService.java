07659a6780edb3f331e793a1dd5401ea165e6b5a
Aaron Long <aaron.long.c@gmail.com>
2017-07-08 22:34:25 -0400

    Resolved more test issues with Mocks

--- server/src/test/java/com/opendatadelaware/feede/service/TestUsersService.java
+++ server/src/test/java/com/opendatadelaware/feede/service/TestUsersService.java
@@ -6,12 +6,19 @@ import com.opendatadelaware.feede.controller.responses.Response;
 import com.opendatadelaware.feede.controller.responses.Success;
 import com.opendatadelaware.feede.controller.utils.RequestBodyMapper;
 import com.opendatadelaware.feede.controller.utils.UserAuthValidator;
+import com.opendatadelaware.feede.dao.UsersDao;
+import com.opendatadelaware.feede.model.Users;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -19,15 +26,26 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Base64;
+import java.util.Optional;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by aaronlong on 7/8/17.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class TestUsersService {
 
   private static byte[] goodBase64;
   private static byte[] badBase64;
   private static final Logger LOGGER = LoggerFactory.getLogger(TestUserController.class);
+
+  @Mock
+  private UsersDao dao;
+
+  @Mock
+  private PasswordEncoder passwordEncoder;
 
   private UsersService service;
 
@@ -50,12 +68,15 @@ public class TestUsersService {
   @Before
   public void setUp() {
     service = new UsersService();
+    service.setDao(dao);
+    service.setPasswordEncoder(passwordEncoder);
   }
 
   @Test
   public void testCreateUserFromRequestSuccess() {
     byte[] json = Base64.getDecoder().decode(goodBase64);
     RequestBodyMapper<UserAuthValidator> auth = RequestBodyMapper.factory(json, UserAuthValidator.class);
+    when(dao.getUserByEmail(anyString())).thenReturn(Optional.empty());
     Response response = service.createUserFromRequest(auth);
     Assert.assertTrue("Success response is returned", response instanceof Success);
   }
