0a7d01d85ae0b63d133a6c23d001b22c00b1f482
Dennis Kalaygian <daeniskalaygian@gmail.com>
2017-07-09 18:06:58 -0400

    Building the successHandler

--- server/src/main/java/com/opendatadelaware/feede/config/jwt/JwtAuthenticationSuccessHandler.java
+++ server/src/main/java/com/opendatadelaware/feede/config/jwt/JwtAuthenticationSuccessHandler.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.opendatadelaware.feede.model.Tokens;
 import com.opendatadelaware.feede.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -36,7 +38,27 @@ public class JwtAuthenticationSuccessHandler implements AuthenticationSuccessHan
                                       Authentication authentication)
           throws IOException, ServletException {
 
-    Tokens accessToken = new Tokens();
+      String principal = (String)authentication.getPrincipal();
+
+      //Create an access Token
+      JwtToken accessToken = JwtToken.createTokenInstance(principal);
+      //Create a refresh token
+
+
+      //Create a map to hold the two tokens that were just created
+      //Place the token in the map
+      //Place the refresh token in the map
+
+      //Set the Http status to OK
+      httpServletResponse.setStatus(HttpStatus.OK.value());
+      //Set the content type of the response
+      httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+      //Write the token values to the mapper variable
+      mapper.writeValue(httpServletResponse.getWriter(), accessToken);
+
+      //Clear the authentication attributes
+      clearAuthenticationAttributes(httpServletRequest);
+
   }
 
   protected final void clearAuthenticationAttributes(HttpServletRequest request) {
