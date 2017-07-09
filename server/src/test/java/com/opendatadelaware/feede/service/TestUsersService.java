26967ca823590c83b171a78e250c715c9e148419
Aaron Long <aaron.long.c@gmail.com>
2017-07-08 20:56:30 -0400

    Test user creation request

--- server/src/test/java/com/opendatadelaware/feede/service/TestUsersService.java
+++ server/src/test/java/com/opendatadelaware/feede/service/TestUsersService.java
@@ -1,6 +1,7 @@
 package com.opendatadelaware.feede.service;
 
 import com.opendatadelaware.feede.controller.TestUserController;
+import com.opendatadelaware.feede.controller.responses.BadRequest;
 import com.opendatadelaware.feede.controller.responses.Response;
 import com.opendatadelaware.feede.controller.responses.Success;
 import com.opendatadelaware.feede.controller.utils.RequestBodyMapper;
@@ -25,17 +26,24 @@ import java.util.Base64;
 public class TestUsersService {
 
   private static byte[] goodBase64;
+  private static byte[] badBase64;
   private static final Logger LOGGER = LoggerFactory.getLogger(TestUserController.class);
 
   private UsersService service;
 
   @BeforeClass
   public static void init() {
+    goodBase64 = readFile("/base64/good.base64");
+    badBase64  = readFile("/base64/bad.base64");
+  }
+
+  private static byte[] readFile(String fileName) {
     try {
-      Path path = Paths.get(TestUsersService.class.getResource("/base64/good.base64").toURI());
-      goodBase64 = Files.readAllBytes(path);
+      Path path = Paths.get(TestUsersService.class.getResource(fileName).toURI());
+      return Files.readAllBytes(path);
     } catch (URISyntaxException | IOException e) {
       LOGGER.error(e.getMessage());
+      return null;
     }
   }
 
@@ -45,11 +53,19 @@ public class TestUsersService {
   }
 
   @Test
-  public void testCreateUserFromRequest() {
+  public void testCreateUserFromRequestSuccess() {
     byte[] json = Base64.getDecoder().decode(goodBase64);
     RequestBodyMapper<UserAuthValidator> auth = RequestBodyMapper.factory(json, UserAuthValidator.class);
     Response response = service.createUserFromRequest(auth);
-    Assert.assertTrue("Right response is returned", response instanceof Success);
+    Assert.assertTrue("Success response is returned", response instanceof Success);
+  }
+
+  @Test
+  public void testCreateUserFromRequestBadRequest() {
+    byte[] json = Base64.getDecoder().decode(badBase64);
+    RequestBodyMapper<UserAuthValidator> auth = RequestBodyMapper.factory(json, UserAuthValidator.class);
+    Response response = service.createUserFromRequest(auth);
+    Assert.assertTrue("BadRequest response is returned", response instanceof BadRequest);
   }
 
 }
