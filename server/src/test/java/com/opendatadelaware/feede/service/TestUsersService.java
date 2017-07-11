8dad7762c92de8e6f2f36e1471b0e567f1b63d15
Dennis Kalaygian <daeniskalaygian@gmail.com>
2017-07-11 17:40:40 -0400

    Writing test for user service

--- server/src/test/java/com/opendatadelaware/feede/service/TestUsersService.java
+++ server/src/test/java/com/opendatadelaware/feede/service/TestUsersService.java
@@ -6,7 +6,9 @@ import com.opendatadelaware.feede.controller.responses.Response;
 import com.opendatadelaware.feede.controller.responses.Success;
 import com.opendatadelaware.feede.controller.utils.RequestBodyMapper;
 import com.opendatadelaware.feede.controller.utils.UserAuthValidator;
+import com.opendatadelaware.feede.controller.utils.UserCredentials;
 import com.opendatadelaware.feede.dao.UsersDao;
+import com.opendatadelaware.feede.model.Users;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -18,8 +20,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.security.auth.login.CredentialException;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -37,6 +41,9 @@ public class TestUsersService {
 
   private static byte[] goodNewUserBase64;
   private static byte[] badNewUserBase64;
+  private static String goodUserLoginBase64;
+  private static String badUserLoginBase64;
+
   private static final Logger LOGGER = LoggerFactory.getLogger(TestUserController.class);
 
   @Mock
@@ -51,6 +58,19 @@ public class TestUsersService {
   public static void init() {
     goodNewUserBase64 = readFile("/base64/goodNewUser.base64");
     badNewUserBase64  = readFile("/base64/badNewUser.base64");
+    goodUserLoginBase64 = readFileAsString("/base64/goodUserLogin.base64");
+    badUserLoginBase64 = readFileAsString("/base64/badUserLogin.base64");
+  }
+
+  private static String readFileAsString(String fileName) {
+    try {
+      URL url = TestUsersService.class.getResource(fileName);
+      byte[] fileAsBytes = Files.readAllBytes(Paths.get(url.toURI()));
+      return new String(fileAsBytes);
+    } catch(URISyntaxException | IOException e) {
+      System.out.println(e.getMessage());
+      return "";
+    }
   }
 
   private static byte[] readFile(String fileName) {
@@ -88,10 +108,14 @@ public class TestUsersService {
   }
   
   @Test
-  public void testValidateUserForLoginSuccess() {
-    byte[] json = Base64.getDecoder().decode(goodNewUserBase64);
+  public void testValidateUserForLoginSuccess() throws CredentialException {
+    UserCredentials creds = new UserCredentials(goodUserLoginBase64);
+    EntityWrapper<Users> user = service.validateUserForLogin(creds);
+    Assert.assertTrue("Checking to see if a valid user object is returned", user != null);
     // Test to make sure that token is returned for a valid user
   }
+
+  // Test for the exception
   
   @Test
   public void testValidateUserForLoginFailure() {
